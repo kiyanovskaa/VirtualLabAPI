146578a6d723dbd0768b05ddeb58d175137539c6
Lenovo <helly.miners121@gmail.com>
2024-11-24 19:09:15 +0200

    added tasks, and cors policy

--- VirtualLabAPI/Program.cs
+++ VirtualLabAPI/Program.cs
@@ -6,6 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost3000", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowLocalhost3000");
 
 app.UseHttpsRedirection();
 
