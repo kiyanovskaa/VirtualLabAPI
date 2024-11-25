1d310237d74b002a35634fc3ea42d0af8e93bcaa
Lenovo <helly.miners121@gmail.com>
2024-11-25 03:40:24 +0200

    added test etalon diagram (id=3)

--- VirtualLabAPI/Program.cs
+++ VirtualLabAPI/Program.cs
@@ -24,7 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors("AllowLocalhost3000");
+app.UseCors("AllowLocalhost3000"); 
 
 app.UseHttpsRedirection();
 
